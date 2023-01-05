e29364f7b6a959935870456803d10bb5c4241b47
Falko Schumann <falko.schumann@muspellheim.de>
2023-01-05 20:36:51 +0100

    feat: return JSON from home resource

--- src/main/java/de/muspellheim/restaurant/HelloController.java
+++ src/main/java/de/muspellheim/restaurant/HelloController.java
@@ -1,5 +1,6 @@
 package de.muspellheim.restaurant;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-  @GetMapping("/")
+  @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
   public String index() {
-    return "Hello World!";
+    return """
+      { "message": "Hello World!" }
+    """.trim();
   }
 }
--- src/test/java/de/muspellheim/restaurant/HelloControllerTests.java
+++ src/test/java/de/muspellheim/restaurant/HelloControllerTests.java
@@ -1,11 +1,13 @@
 package de.muspellheim.restaurant;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -15,8 +17,12 @@ class HelloControllerTests {
   @Autowired private MockMvc mvc;
 
   @Test
-  void homeIsOk() throws Exception {
-    mvc.perform(MockMvcRequestBuilders.get("/")) //
-        .andExpect(status().is2xxSuccessful());
+  void homeReturnsJson() throws Exception {
+    var request = MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON);
+
+    var response = mvc.perform(request);
+
+    response.andExpect(status().is2xxSuccessful());
+    response.andExpect(content().contentType(MediaType.APPLICATION_JSON));
   }
 }
