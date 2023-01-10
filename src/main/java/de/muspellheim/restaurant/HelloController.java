63c72818f13b296f7b4608db264915cf15aeb531
Falko Schumann <falko.schumann@muspellheim.de>
2023-01-10 21:51:45 +0100

    build: configure Checkstyle

--- src/main/java/de/muspellheim/restaurant/HelloController.java
+++ src/main/java/de/muspellheim/restaurant/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/** Stub controller. */
 @RestController
 public class HelloController {
 
