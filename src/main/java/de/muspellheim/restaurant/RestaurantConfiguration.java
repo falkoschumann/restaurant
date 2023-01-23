15fc1a911f05b9c5110cf1932858f0823675f51e
Falko Schumann <falko.schumann@muspellheim.de>
2023-01-23 07:34:32 +0100

    fix: use non-local time format for parsing the configuration

--- src/main/java/de/muspellheim/restaurant/RestaurantConfiguration.java
+++ src/main/java/de/muspellheim/restaurant/RestaurantConfiguration.java
@@ -6,11 +6,14 @@ import java.util.List;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
 @ConfigurationProperties("restaurant")
 public class RestaurantConfiguration {
+  @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
   private LocalTime opensAt;
+
   private LocalTime lastSeating;
   private Duration seatingDuration;
   private List<TableConfiguration> tables;
